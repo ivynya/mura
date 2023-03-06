3b525fd464cef199be09a19a0683de134cd54d0b
ivynya <ivy@ivy.direct>
2023-03-06 10:54:31 -0800

    feat: Add share, add buttons

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,4 +1,23 @@
 
+<script lang="ts">
+  function share() {
+    if (navigator.share) {
+      navigator
+        .share({
+          title: "Schedule with Ivy on Mura",
+          text: "Schedule a meeting with Ivy on Mura",
+          url: "https://mura.ivy.direct/",
+        })
+        .then(() => console.log("Successful share"))
+        .catch((error) => console.log("Error sharing", error));
+    }
+  }
+
+  function createNewMura() {
+    // TODO: Create a new Mura
+  }
+</script>
+
 <header>
   <h1>
     <svg
@@ -10,8 +29,7 @@
       width="100%"
       xml:space="preserve"
       xmlns="http://www.w3.org/2000/svg"
-      xmlns:xlink="http://www.w3.org/1999/xlink"
-    >
+      xmlns:xlink="http://www.w3.org/1999/xlink">
       <defs />
       <g id="Layer-1">
         <g opacity="1">
@@ -107,11 +125,26 @@
     </svg>
     <span>MURA</span>
   </h1>
-  <nav />
+  <div>
+    <button on:click={share}>
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-up" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M3.5 6a.5.5 0 0 0-.5.5v8a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5v-8a.5.5 0 0 0-.5-.5h-2a.5.5 0 0 1 0-1h2A1.5 1.5 0 0 1 14 6.5v8a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5v-8A1.5 1.5 0 0 1 3.5 5h2a.5.5 0 0 1 0 1h-2z"/>
+        <path fill-rule="evenodd" d="M7.646.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 1.707V10.5a.5.5 0 0 1-1 0V1.707L5.354 3.854a.5.5 0 1 1-.708-.708l3-3z"/>
+      </svg>
+    </button>
+    <button>
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+      </svg>
+    </button>
+  </div>
 </header>
 
 <style lang="scss">
 	header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 		padding: 1rem 2rem;
     padding-top: 0.25rem;
 
@@ -127,5 +160,34 @@
 			font-size: 2em;
 			margin: 0;
 		}
+
+    div {
+      display: flex;
+      align-items: center;
+      column-gap: 2rem;
+    }
+
+    button {
+      background: none;
+      border: none;
+      border-radius: 0.5rem;
+      color: #f5fff1;
+      display: grid;
+      place-items: center;
+
+      font-family: inherit;
+      cursor: pointer;
+      width: 2.5rem;
+      height: 2.5rem;
+
+      svg {
+        height: 1.75rem;
+        width: 1.75rem;
+      }
+
+      &:hover {
+        background: #f5fff133;
+      }
+    }
 	}
 </style>
