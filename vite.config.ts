6f1820cbfd05811dca3a77132ec3ce2b3eacd760
ivynysa <ivy@ivy.direct>
2025-06-14 22:24:38 -0700

    use modern sass api

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,16 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	css: {
+		preprocessorOptions: {
+			scss: {
+				api: 'modern'
+			}
+		}
+	},
+	server: {
+		port: 5175,
+		strictPort: false
+	}
 });
